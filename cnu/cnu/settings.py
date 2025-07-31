43874f2208350d12bfeb64f6c4fa742d829bf8d0
Dorcea Matei <dorceamatei010@gmail.com>
2025-07-31 18:21:45 +0300

    fix

--- cnu/cnu/settings.py
+++ cnu/cnu/settings.py
@@ -150,7 +150,7 @@ CKEDITOR_CONFIGS = {
     },
 }
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'colegiulunireatr'
 AWS_S3_SIGNATURE_NAME = 's3v4',
 AWS_S3_REGION_NAME = 'eu-central-1'
